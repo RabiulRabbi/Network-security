251edbd72c8084ad71e2927ac617c547e3566b79
RabiulRabbi <67532904+RabiulRabbi@users.noreply.github.com>
2023-03-19 07:01:55 +0600

    Add files via upload

---
+++ monoSub(file-but-not-map).cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+char encMap[256], decMap[256];
+
+string encryption_monosub(string plainText);
+string decryption_monosub(string cipherText);
+
+int main()
+{
+    freopen("monosubInput.txt", "r", stdin);
+
+    for(int i=0; i<26; i++)
+    {
+        char a, b;
+        cin >> a >> b;
+
+        encMap[a] = b;
+        decMap[b] = a;
+    }
+    string plainText;
+    cin >> plainText;
+
+    string cipherText = encryption_monosub(plainText);
+    cout << "The cipherText is: " << cipherText << "\n";
+
+    string decipherText = decryption_monosub(cipherText);
+    cout << "The decipherText is: " << decipherText << "\n";
+
+    return 0;
+}
+
+string encryption_monosub(string plainText)
+{
+    string cipherText = "";
+
+    for(int i=0; i<plainText.size(); i++)
+    {
+        cipherText += encMap[plainText[i]];
+    }
+
+    return cipherText;
+}
+
+string decryption_monosub(string cipherText)
+{
+    string decipherText = "";
+
+    for(int i=0; i<cipherText.size(); i++)
+    {
+        decipherText += decMap[cipherText[i]];
+    }
+    return decipherText;
+}
+
+
